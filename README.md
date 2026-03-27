7d92f73c085f2ef52b922e5229ffe152233baf1b
Viktoria Safonova <vercaz132@gmail.com>
2026-03-27 15:36:43 +0300

    kr52

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#KR 1
+#KR 1 (кр 2 ниже)
 
 ##1.1
 ![alt text](image.png)
@@ -20,3 +20,27 @@
 
 #Весь код кр 
 ![alt text](image-7.png)
+
+
+#KR 2
+
+##3.1
+![alt text](image-3.1.1.png)
+![alt text](image-3.1.2.png)
+
+##3.2
+![alt text](image.3.2.1.png)
+![alt text](image-3.2.2.png)
+
+##5.1-5.3
+![alt text](image-5.3.1.png)
+![alt text](image-5.3.2.png)
+
+
+##5.4-5.5
+![alt text](image-5.4.1.png)
+![alt text](image-5.4.2.png)
+
+##5.5
+![alt text](image-5.5.1.png)
+
--- models.py
+++ models.py
@@ -1,9 +1,40 @@
-from pydantic import BaseModel, EmailStr, Field, ConfigDict
+from pydantic import BaseModel, EmailStr, Field, field_validator
+from typing import Optional
+import re
+from typing import Annotated
 
-class User(BaseModel):
+class UserCreate(BaseModel):
     name: str
-    id: int
+    email: EmailStr
+    age: Optional[int] = Field(None, gt=0)
+    is_subscribed: Optional[bool] = False
 
-class Feedback(BaseModel):
+class Product(BaseModel):
+    product_id: int
     name: str
-    message: str
+    category: str
+    price: float
+
+class CommonHeaders(BaseModel):
+    # Используем Alias, чтобы Pydantic понимал заголовки с дефисами
+    user_agent: str = Field(..., alias="User-Agent")
+    accept_language: str = Field(..., alias="Accept-Language")
+
+    @field_validator("accept_language")
+    @classmethod
+    def validate_accept_language(cls, v: str):
+        # Регулярка теперь разрешает буквы, цифры и спецсимволы заголовка
+        pattern = r"^[a-zA-Z0-9\-_,;=.* ]+$"
+        if not re.match(pattern, v):
+            raise ValueError("Invalid Accept-Language format")
+        return v
+
+# from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+# class User(BaseModel):
+#     name: str
+#     id: int
+
+# class Feedback(BaseModel):
+#     name: str
+#     message: str
